f119b4d7033d992b2afcf435737e6938f1a3c891
User <koboshove@gmail.com>
2022-02-12 18:39:47 +0600

    hm13

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -3,12 +3,35 @@ package peaksoft;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+//        1) Cat деген POJO класс тузунуз
+//        2) AppConfig класстын ичинде cat деген класска бин тузунуз
+//        3) Ал бинди ар бир чакырганда жаны объект тузуло тургандай кылып настройка кылыныз
+//        4) App классында HelloWorldду дагы бир жолу жана Catти эки жолу чакырыныз.
+//        Аларды ссылкасы менен салыштырып коруп жообун консольго чыгарыныз.
+//        5) Тестти комментарийден чыгарып иштетип корунуз
+
+//
+//
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean==bean1);
+
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        System.out.println(cat==cat1);
+
+
+
+
+    ;
+
+
+
+
     }
 }
